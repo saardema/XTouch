0b0598f16cf5abbed952e697281206eca2ee1820
Sam Aardema <sam.aardema@gmail.com>
2025-12-16 03:32:00 +0100

    Update mapping

--- motu_xtouch/mapping.py
+++ motu_xtouch/mapping.py
@@ -1,4 +1,4 @@
-CHANNELS = [0, 2, 3, 4, 6, 8, 14, 16]
+CHANNELS = [8, 9, 10, 12, 14, 14, 14, 16]
 
 FADER_CC = {
     1: 'chan/' + str(CHANNELS[0]) + '/matrix/fader',
@@ -40,9 +40,7 @@ RECORD_ARM_NOTE = {
     34: 'chan/' + str(CHANNELS[2]) + '/matrix/aux/6/send',
     35: 'chan/' + str(CHANNELS[3]) + '/matrix/aux/6/send',
     36: 'chan/' + str(CHANNELS[4]) + '/matrix/aux/6/send',
-    # Disabled to prevent feedback
-    # 37: ' + str(CHANNELS[5]) + 'han/{}/matrix/aux/6/send',
-    # 38: 'group/0/matrix/aux/6/send',
+    37: 'chan/' + str(CHANNELS[5]) + '/matrix/aux/6/send',
     38: 'chan/' + str(CHANNELS[6]) + '/matrix/aux/6/send',
     39: 'chan/' + str(CHANNELS[7]) + '/matrix/aux/6/send',
 }
@@ -59,7 +57,7 @@ MUTE_NOTE = {
 }
 
 SIDE_ROTARY_CC = {
-    21: 'monitor/0/matrix/fader', # Headphones
+    21: 'monitor/0/matrix/fader',  # Headphones
     23: 'aux/2/matrix/fader',     # JBL
     25: 'aux/4/matrix/fader',     # Sub
     20: 'aux/5/matrix/fader',     # FX send
@@ -68,10 +66,10 @@ SIDE_ROTARY_CC = {
 }
 
 SIDE_ROTARY_NOTE = {
-    11: 'monitor/0/matrix/fader', # Headphones
+    11: 'monitor/0/matrix/fader',  # Headphones
     13: 'aux/2/matrix/fader',     # JBL
     15: 'aux/4/matrix/fader',     # Sub
-    10:  'aux/5/matrix/fader',     # FX send
-    14:  'group/0/matrix/fader',   # Inputs
+    10: 'aux/5/matrix/fader',     # FX send
+    14: 'group/0/matrix/fader',   # Inputs
     12: 'aux/6/matrix/fader',     # Record
 }
